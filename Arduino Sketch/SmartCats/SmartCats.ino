32149bb76180c9465a81450ab5ab15684b32a2dd
latterarrays <latterarrays@gmail.com>
2022-05-11 15:48:16 -0400

    Fixing arduino code

--- Arduino Sketch/SmartCats/SmartCats.ino
+++ Arduino Sketch/SmartCats/SmartCats.ino
@@ -8,7 +8,6 @@
 #include "arduino_secrets.h" 
 #include <Rfid134.h>
 
-
 #define RST_PIN 7
 #define BLINK 6
 
@@ -22,10 +21,9 @@ int wifi_timeout_ms = 5000;
 int ws_message_size = 0;
 int last_server_ping = 0;
 
-// Variables for RFID sensor reading (WL-134)
 
 
-int this_sensor = 2;
+int this_sensor = 1;
 
 // Helper functions for operating the LED
 void slow_blink() {
@@ -65,6 +63,61 @@ void transmit_message(int message) {
   client.print(message);
   client.endMessage();
 }
+
+
+// Variables for RFID sensor reading (WL-134)
+
+
+// implement a notification class,
+// its member methods will get called 
+//
+class RfidNotify
+{
+public:
+  static void OnError(Rfid134_Error errorCode)
+  {
+    // see Rfid134_Error for code meaning
+    Serial.println();
+    Serial.print("Com Error ");
+    Serial.println(errorCode);
+  }
+
+  static void OnPacketRead(const Rfid134Reading& reading)
+  {
+    char temp[8];
+
+    Serial.print("TAG: ");
+
+    // since print doesn't support leading zero's, use sprintf
+    sprintf(temp, "%03u", reading.country);
+    Serial.print(temp);   
+
+    Serial.print(" ");
+    
+    // since print doesn't support leading zero's, use sprintf
+    // since sprintf with AVR doesn't support uint64_t (llu/lli), use /% trick to
+    // break it up into equal sized leading zero pieces
+    sprintf(temp, "%06lu", static_cast<uint32_t>(reading.id / 1000000));
+    Serial.print(temp); 
+    sprintf(temp, "%06lu", static_cast<uint32_t>(reading.id % 1000000));
+    Serial.print(temp); 
+
+    Serial.print(" ");
+    if (reading.isData)
+    {
+        Serial.print("data");
+    }
+    if (reading.isAnimal)
+    {
+        Serial.print("animal");
+    }
+    Serial.println();
+    transmit_message(reading.id);
+    reset_rfid_scanner();
+
+  }
+
+};
 
 Rfid134<HardwareSerial, RfidNotify> rfid(Serial1);
 
